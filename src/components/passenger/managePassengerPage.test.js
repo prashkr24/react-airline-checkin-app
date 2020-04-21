2bff61540d1ddaa669cbb012e0c98f58421b90f7
Shijith Kandirithy Mohanan <kandirithy.m@pg.com>
2020-04-21 13:45:02 +0530

    Test file created

--- src/components/passenger/managePassengerPage.test.js
+++ src/components/passenger/managePassengerPage.test.js
@@ -1,21 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-it('should pass', () => {
-    expect(true).toEqual(true)
-})
-
 import React from 'react'
+import ReactDOM from 'react-dom'
 import ManagePassengerPage from './managePassengerPage'
-import renderer from 'react-test-renderer'
-import { passengers } from '../../../tools/mockData'
 
-it("sets submit button label 'Saving...' when saving is true", () => {
-    const tree = renderer.create(
-        <ManagePassengerPage
-            passengers={passengers}
-            loadPassengers={jest.fn()}
-            savePassenger={jest.fn()}
-            deletePassenger={jest.fn()}
-        />
-    )
-    expect(tree).toMatchSnapshot()
+it('Render without creashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render('<ManagePassengerPage/>', div)
 })
